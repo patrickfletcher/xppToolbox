241c02647b335ded51e57c4296513ec7c92458a4
Patrick Fletcher <patrick.allen.fletcher@gmail.com>
2020-09-27 12:27:22 -0400

    update ode2m demos

--- ode_euler.m
+++ ode_euler.m
@@ -1,27 +1,19 @@
 function [t,Y,DY,AUX,W]=ode_euler(getRHS,tspan,dt,y0,nWiener)
 %ODE_EULER A simple euler solver supporting Wiener variables.
-%
-% 
-
-% ti=tspan(1);
-yi=y0(:);
-nSteps=ceil( (tspan(2)-tspan(1))/dt )+1;
-% t=zeros(nSteps,1);
-Y=zeros(nSteps,length(y0));
 
 %initial values
-% t(1)=ti;
-t=tspan(1):dt:nSteps*dt;
-Y(1,:)=yi(:)';
-
+t=tspan(1):dt:tspan(2);
+nSteps=length(t)-1;
+Y=zeros(nSteps+1,length(y0));
+Y(1,:)=y0(:)';
 
 w=randn(nWiener,1)/sqrt(dt);
 [dyi, tmpaux]=getRHS(t(1), Y(1,:), w);
 
 if nargout>2
-DY=zeros(nSteps,length(y0));
-W=zeros(nSteps,length(y0));
-AUX=zeros(nSteps,length(tmpaux));
+DY=zeros(nSteps+1,length(y0));
+W=zeros(nSteps+1,length(y0));
+AUX=zeros(nSteps+1,length(tmpaux));
 
 DY(1,:)=dyi(:)';
 AUX(1,:)=tmpaux(:)';
@@ -29,15 +21,12 @@ W(1,:)=w;
 end
 
 %timestep loop
+yi=y0(:);
 for i=2:nSteps+1
     
     w=randn(nWiener,1)/sqrt(dt);
     [k1, aux]=getRHS(t(i), yi, w); 
     yi=yi+k1*dt;
-%     ti=t(1)+(i-1)*dt;
-    
-    %store
-%     t(i,1)=ti;
     Y(i,:)=yi(:)'; %(:)' forces row vector
     
     if nargout>2
